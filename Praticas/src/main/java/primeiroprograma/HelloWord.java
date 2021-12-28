c56d917fdc6e9892282c767c7ddabfc5c605e582
Geisianny Bernardo <74117146+Geisianny@users.noreply.github.com>
2021-12-28 00:33:30 -0300

    primeiro pratica

---
+++ Praticas/src/main/java/primeiroprograma/HelloWord.java
@@ -0,0 +1,14 @@
+
+package primeiroprograma;
+
+/**
+ *
+ * @author VAIO
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        
+        System.out.println("Hello word, java e POO!");
+    }
+    
+}
