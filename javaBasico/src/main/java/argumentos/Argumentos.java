68777fcc651c5f8663cb0eba045556b4850f89d8
Geisianny Bernardo <74117146+Geisianny@users.noreply.github.com>
2022-01-01 18:44:56 -0300

    praticas

---
+++ javaBasico/src/main/java/argumentos/Argumentos.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package argumentos;
+
+/**
+ *
+ * @author VAIO
+ */
+public class Argumentos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Você digitou " + args[0]);
+    }
+    
+}
