beef7d542ca280b66f10293f9ad46c99360a515d
Geisianny Bernardo <74117146+Geisianny@users.noreply.github.com>
2022-01-05 00:24:39 -0300

    Praticas

---
+++ javaBasico/src/main/java/introducaoAVariaveis/SequenciaEscape.java
@@ -0,0 +1,17 @@
+
+package introducaoAVariaveis;
+
+/**
+ *
+ * @author VAIO
+ */
+public class SequenciaEscape {
+
+    public static void main(String[] args) {
+        
+        System.out.println("\"Hello,World!\"\n\r");
+        
+        System.out.println("1\\4");
+    }
+    
+}
