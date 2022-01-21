90350804e2ad35d428b66051599faa57b9fe13f7
Geisianny Bernardo <74117146+Geisianny@users.noreply.github.com>
2022-01-21 02:33:59 -0300

    correcao

--- javaBasico/src/main/java/UsandoSwitchCase/exercicios/VogalOuConsoante.java
+++ javaBasico/src/main/java/UsandoSwitchCase/exercicios/VogalOuConsoante.java
@@ -13,34 +13,59 @@ public class VogalOuConsoante {
        
         Scanner scan = new Scanner(System.in);
         
-        System.out.println("Digite uma letra(em minuscula): ");
+        System.out.println("Digite uma letra: ");
         String letra = scan.nextLine();
         
-        switch(letra){
-            
-            case "a":
-                System.out.println("É vogal!");
-                break;
-                
-            case "e":
-                System.out.println("É vogal!");
-                break;
-             
-            case "i":
-                System.out.println("É vogal!");
-                break; 
-                
-            case "o":
-                System.out.println("É vogal!");
-                break;
-                
-            case "u":
-                System.out.println("É vogal!");
-                break;
-            
-            default:
-                System.out.println("É consoante!");
-        }
+        if(letra.length() > 1){ // Para verifica o tamanho de caracteres
+            //foram  digitados:
+            System.out.println("Não é uma letra válida!");
+        }else{
+            switch(letra){
+
+                case "a":
+                    System.out.println("É vogal!");
+                    break;
+
+                case "e":
+                    System.out.println("É vogal!");
+                    break;
+
+                case "i":
+                    System.out.println("É vogal!");
+                    break; 
+
+                case "o":
+                    System.out.println("É vogal!");
+                    break;
+
+                case "u":
+                    System.out.println("É vogal!");
+                    break;
+
+                case "A":
+                    System.out.println("É vogal!");
+                    break;
+
+                case "E":
+                    System.out.println("É vogal!");
+                    break;
+
+                case "I":
+                    System.out.println("É vogal!");
+                    break; 
+
+                case "O":
+                    System.out.println("É vogal!");
+                    break;
+
+                case "U":
+                    System.out.println("É vogal!");
+                    break;
+
+                default:
+                    System.out.println("É consoante!");
+            }
+        }    
     }
     
 }
