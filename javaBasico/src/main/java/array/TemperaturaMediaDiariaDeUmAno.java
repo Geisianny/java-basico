41d0db62c438956d97edc7713e2b3c7eec365ffd
Geisianny Bernardo <74117146+Geisianny@users.noreply.github.com>
2022-02-08 22:24:51 -0300

    pratica

--- javaBasico/src/main/java/array/TemperaturaMediaDiariaDeUmAno.java
+++ javaBasico/src/main/java/array/TemperaturaMediaDiariaDeUmAno.java
@@ -24,8 +24,13 @@ public class TemperaturaMediaDiariaDeUmAno {
         System.out.println("O tamanho do array: "+ temperaturas.length);
         
         for(int i =0;i< temperaturas.length;i++){
-            System.out.println("O valor da temperatura do dia "+(i+1) + "é: " 
+            System.out.println("O valor da temperatura do dia "+(i+1) + " é: " 
                     + temperaturas[i]);
+        }
+        
+        for(double temp : temperaturas){
+            System.out.println(temp);
+            
         }
     }
     
